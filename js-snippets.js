6be403723e52ed39a0b31deac2e2818c519b3c67
Ben Rigaud <rigaud89@gmail.com>
2022-01-06 16:17:22 -0400

    first commit

---
+++ js-snippets.js
@@ -0,0 +1,27 @@
+// Stick on scroll 
+const stickThis = document.querySelector(".element-class");
+const startPosition = stickThis.getBoundingClientRect().top + window.pageYOffset;
+window.onscroll = function() {
+    if (window.pageYOffset > startPosition) {
+        stickThis.classList.add('stick');
+    } else {
+        stickThis.classList.remove('stick');
+
+    }
+}
+
+// Scroll into view with offset
+function scrollIntoView(e) {
+    e.preventDefault();
+    const target = document.querySelector('.target-class');
+    const offset = 95;
+    const bodyRect = document.body.getBoundingClientRect().top;
+    const elementRect = target.getBoundingClientRect().top;
+    const elementPosition = elementRect - bodyRect;
+    const offsetPosition = elementPosition - offset;
+
+    window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+    });
+}
